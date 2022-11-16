d2e2611cd06fb8ffd1b0b73319a1f13ff3f6c9e7
yanJuicy <hanjub3@gmail.com>
2022-11-16 20:56:01 +0900

    팀 나가기 조건 추가

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ db = client.TodaysJandi
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('members.html')
@@ -44,11 +45,26 @@ def team_withdrwal():
 
 @app.route("/teams/grasses/<int:team_id>", methods=["GET"])
 def team_info(team_id):
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_id = payload['id']
+    except jwt.ExpiredSignatureError:
+        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+    except jwt.exceptions.DecodeError:
+        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
+
+    login_user = db.members.find_one({'id': user_id}, {'_id': False})
+    login_user_nickname = login_user['nickname']
+    is_team = False
+
     team = db.teams.find_one({'num': team_id})
 
     team_members = []
     for member_nickname in team['members']:
-        team_member = db.members.find_one({'nickname': member_nickname}, {'id': False})
+        if member_nickname == login_user_nickname:
+            is_team = True
+        team_member = db.members.find_one({'nickname': member_nickname}, {'_id': False})
         team_members.append(team_member)
 
     GITHUB_NICKNAME_KEY = 'github'
@@ -65,7 +81,8 @@ def team_info(team_id):
 
     return render_template('team.html',
                            member_infos=member_infos,
-                           team_id=team_id)
+                           team_id=team_id,
+                           is_team=is_team)
 
 
 def get_daily_commit_count(github_nickname):
@@ -74,8 +91,6 @@ def get_daily_commit_count(github_nickname):
     soup = BeautifulSoup(data.text, 'html.parser')
 
     today = datetime.datetime.today().strftime("%Y-%m-%d")
-    print(today)
-
     daily_commit = soup.select_one("rect[data-date='{}']".format(today))
     if daily_commit is None:
         raise ValueError('잘못된 Github nickname')
@@ -97,12 +112,11 @@ def api_register():
     id_list = list(db.members.find({}, {'_id': False}))
     num = len(id_list)
 
-
-
     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
     # 비밀번호를 해쉬로 처리합니다. 암호화하여 저장, 단방향 암호화
-    db.members.insert_one({'id': id_receive, 'pw': pw_hash, 'github' : github_receive ,'nickname': nickname_receive, 'group' : group,'num' : num})
-
+    db.members.insert_one(
+        {'id': id_receive, 'pw': pw_hash, 'github': github_receive, 'nickname': nickname_receive, 'group': group,
+         'num': num})
 
     return jsonify({'result': 'success'})
 
